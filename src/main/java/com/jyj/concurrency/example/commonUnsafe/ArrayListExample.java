d58ac0b8f6ea4b91c916d44ccef5ff1207c7a3c4
jyj <mygoodlifewillbegin@126.com>
2018-12-27 13:36:06 +0800

    不安全的类

---
+++ src/main/java/com/jyj/concurrency/example/commonUnsafe/ArrayListExample.java
@@ -0,0 +1,62 @@
+package com.jyj.concurrency.example.commonUnsafe;
+
+import com.jyj.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @Description:
+ * @Date: 2018/12/27 10:38
+ * @Author:jyj
+ */
+@Slf4j
+@NotThreadSafe
+public class ArrayListExample {
+
+    /**
+     * 请求总数
+     */
+    public static int clientTotal = 5000;
+
+    /**
+     * 同时并发执行的线程数
+     */
+    public static int threadTotal = 200;
+
+    private static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}", list.size());
+    }
+
+    private static void update(int i) {
+        list.add(i);
+    }
+}
---
+++ src/main/java/com/jyj/concurrency/example/commonUnsafe/DateFormatExample1.java
@@ -0,0 +1,61 @@
+package com.jyj.concurrency.example.commonUnsafe;
+
+import com.jyj.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @Description:
+ * @Date: 2018/12/27 10:04
+ * @Author:jyj
+ */
+@Slf4j
+@NotThreadSafe
+public class DateFormatExample1 {
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+
+    /**
+     * 请求总数
+     */
+    public static int clientTotal = 5000;
+
+    /**
+     * 同时并发执行的线程数
+     */
+    public static int threadTotal = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+    }
+
+    private static void update() {
+        try {
+            simpleDateFormat.parse("20181227");
+        } catch (ParseException e) {
+            log.error("parse exception", e);
+        }
+    }
+}
