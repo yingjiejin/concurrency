aa7127790f21bc8309b8b43feac51cf9608bc302
jyj <mygoodlifewillbegin@126.com>
2018-12-28 13:52:35 +0800

    并发容器

---
+++ src/main/java/com/jyj/concurrency/example/concurrent/ConcurrentHashMapExample.java
@@ -0,0 +1,55 @@
+package com.jyj.concurrency.example.concurrent;
+
+import com.jyj.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * @Description:
+ * @Date: 2018/12/27 10:38
+ * @Author:jyj
+ */
+@Slf4j
+@NotThreadSafe
+public class ConcurrentHashMapExample {
+
+    /**
+     * 请求总数
+     */
+    public static int clientTotal = 5000;
+
+    /**
+     * 同时并发执行的线程数
+     */
+    public static int threadTotal = 200;
+
+    private static Map<Integer, Integer> map = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}", map.size());
+    }
+
+    private static void update(int i) {
+        map.put(i, i);
+    }
+}
